c72f66e38fdc269a49f9fcd9347caad0bd2699db
hetaoBackend <hetao7@pku.edu.cn>
2019-09-28 13:13:44 +0900

    FEA: modify the tables

--- server/app/run.py
+++ server/app/run.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 import os
-from flask import Flask, abort, request, jsonify, g, url_for, redirect
+from flask import Flask, abort, request, jsonify, g, url_for
 from flask_sqlalchemy import SQLAlchemy
 from passlib.apps import custom_app_context as pwd_context
 from itsdangerous import (TimedJSONWebSignatureSerializer
                           as Serializer, BadSignature, SignatureExpired)
-from flask_login import (login_user,logout_user, LoginManager, UserMixin, current_user, login_required)
+from flask_login import (login_user,logout_user, LoginManager, UserMixin, current_user)
 from server.app.config import auth
 from server.app.user_list import user_list
 from server.app.user import user
